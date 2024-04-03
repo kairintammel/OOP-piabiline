50296fe86e15812da044f35ffd2634be82d08047
kasskoe <145622921+kasskoe@users.noreply.github.com>
2024-04-03 23:45:00 +0300

    Create Kaart.java

---
+++ Kaart.java
@@ -0,0 +1,19 @@
+// Kaart.java
+public class Kaart {
+    private String kusimus;
+    private String vastus;
+
+    public Kaart(String kusimus, String vastus) {
+        this.kusimus = kusimus;
+        this.vastus = vastus;
+    }
+
+    public String getKusimus() {
+        return kusimus;
+    }
+
+    public String getVastus() {
+        return vastus;
+    }
+}
+
