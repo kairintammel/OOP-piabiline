d1e0b82931010234b45c869ddf1a5a0e3bebd3ee
kasskoe <145622921+kasskoe@users.noreply.github.com>
2024-04-04 17:31:26 +0300

    Update Kaart.java

--- Kaart.java
+++ Kaart.java
@@ -1,17 +1,20 @@
 // Kaart.java
 public class Kaart {
-    private String kusimus;
-    private String vastus;
+    private String kusimus;//privaatne muutuja küsimuste hoidmiseks
+    private String vastus;//privaatne muutuja vastuste hoidmiseks
 
+    //Konstruktor - obkjekti Kaart loomiseks koos küsimuse ja vastusega
     public Kaart(String kusimus, String vastus) {
         this.kusimus = kusimus;
         this.vastus = vastus;
     }
 
+    //Getter kaardi küsimuse saamiseks
     public String getKusimus() {
         return kusimus;
     }
 
+    //Getter kaardi vastuse saamiseks
     public String getVastus() {
         return vastus;
     }
