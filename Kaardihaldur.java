8a7f795ac4f1ad632100c6aec50ea54b47f2755d
kasskoe <145622921+kasskoe@users.noreply.github.com>
2024-05-16 16:42:12 +0300

    Add files via upload

--- Kaardihaldur.java
+++ Kaardihaldur.java
@@ -1,32 +1,20 @@
-// Kaardihaldur.java
+package org.example.loppryhmatoo;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class Kaardihaldur {
-    private List<Kaart> kaardid = new ArrayList<>();
+class Kaardihaldur {
+    private ObservableList<Kaart> kaardid = FXCollections.observableArrayList();
 
-    //lisab uue kaardi
     public void lisaKaart(String kusimus, String vastus) {
         kaardid.add(new Kaart(kusimus, vastus));
     }
 
-    //segab kaarte, kasutades Math.random funktsioon
-    public void segaKaardid() {
-        for (int i = 0; i < kaardid.size(); i++) {
-            int suvalineIndeks = (int) (Math.random() * kaardid.size());
-            Kaart temp = kaardid.get(i);
-            kaardid.set(i, kaardid.get(suvalineIndeks));
-            kaardid.set(suvalineIndeks, temp);
-        }
-    }
-
-
-    //tagastab kaartide listi
-    public List<Kaart> getKaardid() {
+    public List<Kaart> getKõikKaardid() {
         return kaardid;
     }
-
-
-
-
 }
+
+
--- Kaart.java
+++ Kaart.java
@@ -1,22 +1,27 @@
-// Kaart.java
-public class Kaart {
-    private String kusimus;//privaatne muutuja küsimuste hoidmiseks
-    private String vastus;//privaatne muutuja vastuste hoidmiseks
+package org.example.loppryhmatoo;
 
-    //Konstruktor - obkjekti Kaart loomiseks koos küsimuse ja vastusega
+public class Kaart {
+    private String kusimus;
+    private String vastus;
+
+    public void setKusimus(String kusimus) {
+        this.kusimus = kusimus;
+    }
+
+    public void setVastus(String vastus) {
+        this.vastus = vastus;
+    }
+
     public Kaart(String kusimus, String vastus) {
         this.kusimus = kusimus;
         this.vastus = vastus;
     }
 
-    //Getter kaardi küsimuse saamiseks
     public String getKusimus() {
         return kusimus;
     }
 
-    //Getter kaardi vastuse saamiseks
     public String getVastus() {
         return vastus;
     }
 }
-
