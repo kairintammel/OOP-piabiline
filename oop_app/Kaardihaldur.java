40b2056839b4333b9971e6b9a9752bd2df026600
kasskoe <145622921+kasskoe@users.noreply.github.com>
2024-05-16 17:53:08 +0300

    Update Kaardihaldur.java

--- oop_app/Kaardihaldur.java
+++ oop_app/Kaardihaldur.java
@@ -7,12 +7,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Kaardihaldur {
+
+    //List kaartide jaoks
     private ObservableList<Kaart> kaardid = FXCollections.observableArrayList();
 
+    //meetod, kaartide lisamiseks
     public void lisaKaart(String kusimus, String vastus) {
         kaardid.add(new Kaart(kusimus, vastus));
     }
 
+    //tagastab kaarid
     public List<Kaart> getKõikKaardid() {
         return kaardid;
     }
