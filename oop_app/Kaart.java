d101886e6f3457cdc643d11786f6643ff64ecb11
kairintammel <145291243+kairintammel@users.noreply.github.com>
2024-05-16 17:35:05 +0300

    Töötav versioon
    org.example.oop_app kasutast

---
+++ oop_app/Kaart.java
@@ -0,0 +1,27 @@
+package org.example.oop_app;
+
+public class Kaart {
+    private String kusimus;
+    private String vastus;
+
+    public void setKusimus(String kusimus) {
+        this.kusimus = kusimus;
+    }
+
+    public void setVastus(String vastus) {
+        this.vastus = vastus;
+    }
+
+    public Kaart(String kusimus, String vastus) {
+        this.kusimus = kusimus;
+        this.vastus = vastus;
+    }
+
+    public String getKusimus() {
+        return kusimus;
+    }
+
+    public String getVastus() {
+        return vastus;
+    }
+}
---
+++ oop_app/PomodoroTaimer.java
@@ -0,0 +1,50 @@
+package org.example.oop_app;// Pomodoro taimer
+
+import java.util.Scanner;
+
+public class PomodoroTaimer {
+    public static void kutsuVälja() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Mitu 25-5 tsüklit sooviksid käivitada? ");
+
+        int tsükleid = scanner.nextInt();
+
+        System.out.println("Vajuta ENTER taimeri käivitamiseks.");
+
+        int mitmes = 1;
+
+        while (mitmes <= tsükleid) {
+            scanner.nextLine(); // Ootab, et kasutaja vajutaks ENTER
+            tsükleid--;
+
+            System.out.println("Tsükkel " + (mitmes + 1) + " --> Tööaeg 25 minutit.");
+            countdown(25);
+
+            if (mitmes % 3 == 0) { // peale igat neljandat tsüklit pikem paus
+                System.out.println("Pikem paus - 15 minutit.");
+                countdown(15);
+            } else {
+                System.out.println("Paus - 5 minutit.");
+                countdown(5);
+            }
+            mitmes ++;
+        }
+    }
+
+    public static void countdown(int minutid)
+    {
+        int sekundid = minutid * 60;
+
+        while (sekundid > 0) {
+
+            System.out.print("\r" + String.format("%02d:%02d", sekundid / 60, sekundid % 60)); // väljastab järelejäänud aja ekraanile
+            try {
+                Thread.sleep(1000); // ootab 1 sekund enne uut tsüklit
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            sekundid--;
+        }
+        System.out.println("\nAeg!!!");
+    }
+}
